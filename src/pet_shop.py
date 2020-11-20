5523aeb73e45748e245237b1f33d751dc69d5a7a
fukiran <macdorphoto@yahoo.com>
2020-11-20 15:24:10 +0000

    add remove cash

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -2,5 +2,9 @@
 def get_pet_shop_name(pets):
     return pets ["name"]
 
-def get_total_cash(admin1):
-    return admin1 ["admin"] ["total_cash"]
+def get_total_cash(list):
+    return list ["admin"] ["total_cash"]
+
+def add_or_remove_cash(list, cash):
+    list ["admin"] ["total_cash"] += cash
+    
