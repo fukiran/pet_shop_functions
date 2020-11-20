dc3b017052be7a6bda225ce2028b9a034a7846a5
fukiran <macdorphoto@yahoo.com>
2020-11-20 21:42:12 +0000

    customer's pet

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -45,3 +45,9 @@ def get_customer_cash(list):
 
 def remove_customer_cash(list,amount):
     list["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer,pet):
+    customer["pets"].append(pet)
